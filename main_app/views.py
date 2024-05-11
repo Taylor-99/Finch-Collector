b2bbb9f55628c3967121d7943e89cad86a823184
User Taylor <elexus0201@gmail.com>
2024-05-11 16:47:32 -0400

    about

--- main_app/views.py
+++ main_app/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
 
-finches = []
-
 # Create your views here.
 def home(request):
   return render(request, 'home.html')
 
+def about(request):
+    return render(request, 'about.html')

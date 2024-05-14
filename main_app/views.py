6865d129fe17e7f6d4ad102e229c15581aad82b3
User Taylor <elexus0201@gmail.com>
2024-05-13 22:19:29 -0400

    view finches

--- main_app/views.py
+++ main_app/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
 
+finches = []
+
 # Create your views here.
 def home(request):
   return render(request, 'home.html')
 
 def about(request):
     return render(request, 'about.html')
+
+def finches_index(request):
+    return render(request, 'finches/index.html', {'finches': finches})

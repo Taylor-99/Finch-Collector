4eb33269e89524dd546354efef4dc491f03a2591
User Taylor <elexus0201@gmail.com>
2024-05-16 22:18:27 -0400

    view finches with data

--- main_app/views.py
+++ main_app/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 
-finches = []
+finches = [
+   {'species': 'American Goldfinch', 'description': 'A vibrant symbol of summer, sports bright yellow plumage with black wings and a distinct black cap.', 'size': 4.5, 'habitat': "Open Woodlands"},
+   {'species': 'Black Rosy-Finch', 'description': 'A striking alpine bird, boasts a glossy black plumage with contrasting pinkish tones on its wings and belly.', 'size': 6, 'habitat': "Tundra"},
+]
 
 # Create your views here.
 def home(request):
